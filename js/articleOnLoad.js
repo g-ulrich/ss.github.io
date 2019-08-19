94110bcbd8a4614e5c6076d288b01e6008d524a4
GABE <gabriel.ulrich@aol.com>
2019-08-18 22:53:28 -0400

    updte

--- js/articleOnLoad.js
+++ js/articleOnLoad.js
@@ -1,8 +1,8 @@
 $(document).ready(function(){
-                $("#web-header").load("common/header.html");
-                $("#web-rightSideBar").load("common/search.html");
-                $("#web-articlesForIndex").load("common/articlesIndex.html");
-                $("#web-footer").load("common/footer.html");
+                $("#web-header").html("common/header.html");
+                $("#web-rightSideBar").html("common/search.html");
+                $("#web-articlesForIndex").html("common/articlesIndex.html");
+                $("#web-footer").html("common/footer.html");
               $("#articleSearch").on("keyup", function() {
                 var value = $(this).val().toLowerCase();
                 $("#articleList li").filter(function() {
