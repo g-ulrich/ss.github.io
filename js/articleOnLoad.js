3d282037524e89154ac357b8885998daa0de2426
GABE <gabriel.ulrich@aol.com>
2019-08-18 23:19:52 -0400

    simplying

--- js/articleOnLoad.js
+++ js/articleOnLoad.js
@@ -3,10 +3,5 @@ $(document).ready(function(){
                 $("#web-rightSideBar").html("common/search.html");
                 $("#web-articlesForIndex").html("common/articlesIndex.html");
                 $("#web-footer").html("common/footer.html");
-              $("#articleSearch").on("keyup", function() {
-                var value = $(this).val().toLowerCase();
-                $("#articleList li").filter(function() {
-                  $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1);
-                });
-             });
+
 	});
