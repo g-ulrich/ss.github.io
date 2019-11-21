ac6a7302601fb13a84fb19a9d782f558c7ffca9a
GABE <gabriel.ulrich@aol.com>
2019-11-21 18:50:27 -0500

    Article
    Setting up the post page js css html

--- js/post.js
+++ js/post.js
@@ -1,7 +1,7 @@
 $(document).ready(function(){
     $("#web-header").load("../common/post-header.html");
     $("#web-rightSideBar").load("../common/search.html");
-    $("#web-resources").load("../common/resources.html");
+    $("#web-resources").load("../common/post-resources.html");
     $("#web-footer").load("../common/post-footer.html");
     $("#articleSearch").on("keyup", function() {
         var value = $(this).val().toLowerCase();
