8e5eb959c8e115eab289f24de48d52b0bdcbb990
GABE <gabriel.ulrich@aol.com>
2019-11-21 20:10:37 -0500

    articles home page
    design and some functions

--- js/articleshome.js
+++ js/articleshome.js
@@ -1,5 +1,5 @@
 $(document).ready(function(){
-    $("#web-rightSideBar").load("common/articlesHomeSearch.html");
+    $("#web-search").load("common/search.html");
     $("#web-resources").load("common/resources.html");
     $("#articleSearchInput").on("keyup", function() {
         var value = $(this).val().toLowerCase();
